a05ab8b61a0a086b5e1b5e66a2d321e60b64150b
andrei <buvchik@gmail.com>
2020-01-28 01:14:04 +0300

    //five task complite

--- src/jc01_2020/buvin/lesson03/Task5.java
+++ src/jc01_2020/buvin/lesson03/Task5.java
@@ -19,7 +19,7 @@ public class Task5 {
 			for (int j = 1; j <=i ; j++) {
 				if (i%j==0 && i!=j) sum=sum+j;
 			}
-			if (sum==i) System.out.println(i);
+			if ( sum==i) System.out.println(i);
 
 		}
 	}
