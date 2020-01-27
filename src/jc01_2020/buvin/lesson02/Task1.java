1befc112dad3d8b5134a9d6aac7d08c4091f2cfc
igor <gob1in@Mail.ru>
2020-01-27 20:55:53 +0300

    //добавил пункт default в case

--- src/jc01_2020/buvin/lesson02/Task1.java
+++ src/jc01_2020/buvin/lesson02/Task1.java
@@ -1,4 +1,4 @@
-package jc01_2020.buvin.lesson02;
+package jc01_2020.lesson02;
 
 /*
  *
@@ -18,6 +18,25 @@ import java.util.Scanner;
 public class Task1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int numeric = scanner.nextInt();
-	}
+        int numeric = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int numeric = scanner.nextInt();
+        switch(numeric)
+        {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+
+            default:
+                System.out.println("Другое число");
+                break;
+        }
+
+    }
 }
