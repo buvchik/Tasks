45036ee9d108efa1507e6ad575acf8f71b6616f0
igor <gob1in@Mail.ru>
2020-01-27 20:55:53 +0300

    //добавил пункт default в case

--- src/jc01_2020/buvin/lesson02/Task1.java
+++ src/jc01_2020/buvin/lesson02/Task1.java
@@ -1,31 +1,42 @@
-package jc01_2020.buvin.lesson02;
+package jc01_2020.lesson02;
 
 /*
-*
-* Какое из чисел 1, 2 или 3 введено
-*
-*/
+ *
+ * Какое из чисел 1, 2 или 3 введено
+ * С клавиатуры вводится число. Вывести на экран какое число введено в соответствии с форматом вывода
+ *
+ * Формат вывода (ошибки недопустимы):
+ * "Число 1"
+ * "Число 2"
+ * "Число 3"
+ * "Другое число"
+ *
+ */
 
 import java.util.Scanner;
 
 public class Task1 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        int numeric = scanner.nextInt();
         Scanner scanner = new Scanner(System.in);
         int numeric = scanner.nextInt();
         switch(numeric)
         {
             case 1:
-                System.out.println("Число 1");
+                System.out.println("1");
                 break;
             case 2:
-                System.out.println("Число 2");
+                System.out.println("2");
                 break;
             case 3:
-                System.out.println("Число 3");
+                System.out.println("3");
                 break;
+
             default:
                 System.out.println("Другое число");
                 break;
         }
+
     }
 }
