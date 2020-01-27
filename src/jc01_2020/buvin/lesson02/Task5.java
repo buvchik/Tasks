4876424fa355449be208edd77894c03bff186511
buvchik <buvchik@gmail.com>
2020-01-27 21:23:55 +0300

    //добавил пункт default в case

--- src/jc01_2020/buvin/lesson02/Task5.java
+++ src/jc01_2020/buvin/lesson02/Task5.java
@@ -1,27 +1,23 @@
-package jc01_2020.buvin.lesson02;
+package jc01_2020.lesson02;
 
 /*
-*
-* Определить, является ли год, который ввел пользователем, високосным или невисокосным.
-*
-* Високосные года делятся нацело на 4.
-* Однако из этого правила есть исключение: столетия, которые не делятся нацело на 400, високосными не являются.
-*
-* 1980 - Високосный
-* 1978 - Обычный
-*
-*/
+ *
+ * Определить, является ли год, который ввел пользователем, високосным или невисокосным.
+ *
+ * Високосные года делятся нацело на 4.
+ * Однако из этого правила есть исключение: столетия, которые не делятся нацело на 400, високосными не являются.
+ *
+ * Формат вывода (ошибки недопустимы):
+ * "Високосный"
+ * "Обычный"
+ *
+ */
 
 import java.util.Scanner;
 
 public class Task5 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int year = scanner.nextInt();
-        if (year%400==0 ) System.out.println("Високосный");
-        if (year%400!=0 && year%100==0) System.out.println("Обычный");
-        else if(year%4==0) System.out.println("Високосный");
-        else System.out.println("Обычный");
-
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int year = scanner.nextInt();
+	}
 }
