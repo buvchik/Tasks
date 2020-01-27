7df7832268b6dcf772cc8851effe328683bf70a5
andrei <buvchik@gmail.com>
2020-01-28 01:05:27 +0300

    //four task complite

--- src/jc01_2020/buvin/lesson03/Task4.java
+++ src/jc01_2020/buvin/lesson03/Task4.java
@@ -12,8 +12,16 @@ package jc01_2020.buvin.lesson03;
 import java.util.Scanner;
 
 public class Task4 {
+	public static int fact(int n){
+			int rez =1;
+			if (n==1||n==0)return rez;
+			rez = n*fact(n-1);
+            return rez;
+    }
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int numeric = scanner.nextInt();
+		if(numeric>0)
+		System.out.println(fact(numeric));
 	}
 }
