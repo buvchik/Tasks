809f122dd640c511649bdbb2243404da147d0bbc
andrei <buvchik@gmail.com>
2020-01-28 08:20:33 +0300

    //fix error

--- src/jc01_2020/buvin/lesson03/Task5.java
+++ src/jc01_2020/buvin/lesson03/Task5.java
@@ -14,7 +14,7 @@ package jc01_2020.buvin.lesson03;
 public class Task5 {
 	public static void main(String[] args) {
 
-		for (int i = 0; i <=1000; i++) {
+		for (int i = 1; i <=10000; i++) {
 			int sum=0;
 			for (int j = 1; j <=i ; j++) {
 				if (i%j==0 && i!=j) sum=sum+j;
