5ca2315863dbd7240f6ee52befa6de525b4af049
andrei <buvchik@gmail.com>
2020-01-28 07:57:29 +0300

    //seven task complite

--- src/jc01_2020/buvin/lesson03/Task7.java
+++ src/jc01_2020/buvin/lesson03/Task7.java
@@ -14,5 +14,12 @@ public class Task7 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int length = scanner.nextInt();
+		for (int i = 0; i < length; i++) {
+			System.out.println();
+			for (int j = 0; j < length; j++) {
+				if (i==j||i==(length-j-1)) System.out.print("*");
+				else System.out.print("-");
+			}
+		}
 	}
 }
