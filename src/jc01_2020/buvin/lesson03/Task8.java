78cbce55ea640787f78824082c4321e109820e74
andrei <buvchik@gmail.com>
2020-01-28 08:03:10 +0300

    //ninth task complite

--- src/jc01_2020/buvin/lesson03/Task8.java
+++ src/jc01_2020/buvin/lesson03/Task8.java
@@ -10,6 +10,11 @@ package jc01_2020.buvin.lesson03;
 
 public class Task8 {
 	public static void main(String[] args) {
-//        System.out.println("");
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.println(i+"*"+j+" = " +i*j);
+			}
+			System.out.println();
+		}
 	}
 }
