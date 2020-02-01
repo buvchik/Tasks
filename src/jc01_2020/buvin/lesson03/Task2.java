cb33d743cee3de7521e0347b8287f8cbe25a9253
andrei <buvchik@gmail.com>
2020-02-01 18:41:55 +0300

    //

--- src/jc01_2020/buvin/lesson03/Task2.java
+++ src/jc01_2020/buvin/lesson03/Task2.java
@@ -14,18 +14,21 @@ package jc01_2020.buvin.lesson03;
 import java.util.Scanner;
 
 public class Task2 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int secret = (int) (Math.random() * 100 + 1);
-		int enterNum=0,col = 0;
-		while (secret!=enterNum && col <10 ){
-			enterNum = scanner.nextByte();
-			if (enterNum<0){ System.out.println("Число меньше 0");}
-			else if (enterNum>secret){System.out.println("Ввели больше");}
-			else if (enterNum<secret) {System.out.println("Ввели меньше");}
-			else if (enterNum==secret) System.out.println("Угадали!");
-			col++;
-		}
-		if (col >=10) System.out.println("Количество попыток превышено");
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int secret = (int) (Math.random() * 100 + 1);
+        int enterNum = 0, col = 0;
+        while (secret != enterNum && col < 10) {
+            enterNum = scanner.nextByte();
+            if (enterNum < 0) {
+                System.out.println("Число меньше 0");
+            } else if (enterNum > secret) {
+                System.out.println("Ввели больше");
+            } else if (enterNum < secret) {
+                System.out.println("Ввели меньше");
+            } else if (enterNum == secret) System.out.println("Угадали!");
+            col++;
+        }
+        if (col >= 10) System.out.println("Количество попыток превышено");
+    }
 }
