28b9d948cb9c69435ad1cb2d85f71ab186151b8d
andrei <buvchik@gmail.com>
2020-02-01 16:24:40 +0300

    //готово

--- src/jc01_2020/buvin/lesson02/Task2.java
+++ src/jc01_2020/buvin/lesson02/Task2.java
@@ -3,11 +3,6 @@ package jc01_2020.buvin.lesson02;
 /*
 *
 * Делится ли введенное число на 3 без остатка
-* С клавиатуры вводится число. Вывести на экран делится ли оно на 3 без остатка
-*
-* Формат вывода (ошибки недопустимы):
-* "Делится"
-* "Не делится"
 *
 */
 
@@ -17,5 +12,7 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long numeric = scanner.nextLong();
+        if (numeric%3==0) System.out.println("Да");
+        else System.out.println("Нет");
     }
 }
