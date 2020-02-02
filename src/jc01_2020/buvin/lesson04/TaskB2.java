185eaff08203c8477d914bdaaa86bc2680b79016
Andrei <buvchik@gmail.com>
2020-02-02 12:03:31 +0300

    //done

--- src/jc01_2020/buvin/lesson04/TaskB2.java
+++ src/jc01_2020/buvin/lesson04/TaskB2.java
@@ -1,4 +1,4 @@
-package jc01_2020.lesson04;
+package jc01_2020.buvin.lesson04;
 
 /*
  *
@@ -15,13 +15,24 @@ package jc01_2020.lesson04;
 import java.util.Scanner;
 
 public class TaskB2 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int[][] array = new int[4][3];
-		for (int i = 0; i < array[0].length; i++) {
-			for (int j = 0; j < array.length; j++) {
-				array[j][i] = scanner.nextInt();
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] array = new int[4][3];
+        for (int i = 0; i < array[0].length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[j][i] = scanner.nextInt();
+            }
+        }
+        int min = array[0][0];
+        for (int[] ints : array) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (ints[j] < min) min = ints[j];
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (array[i][j] == min) System.out.println("строка: " + j + ", столбец: " + i);
+            }
+        }
+    }
 }
