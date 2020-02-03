365eb2ba9439bdd9157577d3284122d88867eb4e
andrei <buvchik@gmail.com>
2020-02-03 13:45:09 +0300

    //

--- src/jc01_2020/buvin/lesson03/Task1.java
+++ src/jc01_2020/buvin/lesson03/Task1.java
@@ -1,4 +1,4 @@
-package jc01_2020.lesson03;
+package jc01_2020.buvin.lesson03;
 
 /*
  *
@@ -11,9 +11,18 @@ package jc01_2020.lesson03;
 
 import java.util.Scanner;
 
+
 public class Task1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		long input = scanner.nextLong();
+		int max=0;
+		String str = String.valueOf(input);
+		for (int i = 0; i < str.length() ; i++) {
+			if (str.chars().toArray()[i]>max){
+				max = str.chars().toArray()[i];
+			}
+		}
+		System.out.println((char)max);
 	}
 }
