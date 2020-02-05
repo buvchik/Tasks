6a3545163e36323e5a87ac0f6ad628fc8c4b6d9d
andrei <buvchik@gmail.com>
2020-02-06 01:48:00 +0300

    //хз

---
+++ src/jc01_2020/buvin/lesson05/TaskB2.java
@@ -0,0 +1,37 @@
+package jc01_2020.buvin.lesson05;
+
+/*
+ *
+ * Проверить, что введенная фамилия является двойной, т.е. пишется через дефис. Фамилия может быть написана как
+ * кириллицей, так и латиницей, но НЕ одновременно с помощью двух алфавитов. Остальные требования к фамилиям адекватные
+ * (только буквы)
+ *
+ * Вывести в консоль логический результат проверки (boolean)
+ *
+ */
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        boolean result = false;
+
+        Pattern p = Pattern.compile("[a-zA-Z]*[-][[a-zA-Z]&&[\\D]]");
+        Pattern p1 = Pattern.compile("^[а-яА-я]+[-][а-яА-я]");
+
+
+
+        Matcher matcher = p.matcher(str);
+        Matcher matcher1 = p1.matcher(str);
+
+        if (matcher.find()) result = true;
+        else result = matcher1.find();
+
+
+        System.out.println(result);
+    }
+}
