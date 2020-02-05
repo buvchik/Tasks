1c65f372931f818ac4169377886dee5360d1c5ad
andrei <buvchik@gmail.com>
2020-02-05 20:23:40 +0300

    //done

--- src/jc01_2020/buvin/lesson03/Task8.java
+++ src/jc01_2020/buvin/lesson03/Task8.java
@@ -1,4 +1,4 @@
-package jc01_2020.lesson03;
+package jc01_2020.buvin.lesson03;
 
 /*
  *
@@ -11,5 +11,11 @@ package jc01_2020.lesson03;
 public class Task8 {
 	public static void main(String[] args) {
 //        System.out.println("");
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <=9; j++) {
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println();
+		}
 	}
 }
