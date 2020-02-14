59eb9b103b4d646b5d35fe82a6556cb59823faca
buvchik <buvchik@gmail.com>
2020-02-14 19:23:21 +0300

    //done

--- src/jc01_2020/buvin/lesson07/task02/Pawn.java
+++ src/jc01_2020/buvin/lesson07/task02/Pawn.java
@@ -1,4 +1,40 @@
 package jc01_2020.buvin.lesson07.task02;
 
-public class Pawn {
+public class Pawn extends Figure{
+    public Pawn(char vertical, int horizontal) {
+        super(vertical, horizontal);
+    }
+
+    @Override
+    public boolean moveTo(char toVertical, int toHorizontal) {
+
+        int toVerticalInt = charToByte(toVertical);
+        int fromVerticalInt = charToByte(getVertical());
+        if (toHorizontal == getHorizontal() && toVerticalInt == fromVerticalInt) {
+            System.out.println("Точки совпадают!!!");
+            return false;
+        }
+        while (0 < toVerticalInt && toVerticalInt < 9 && 0 < toHorizontal && toHorizontal < 9) {
+
+            if (toHorizontal > getHorizontal()) {
+                if (toVerticalInt > fromVerticalInt) {
+                    toVerticalInt--;
+                    toHorizontal--;
+                } else if (toVerticalInt < fromVerticalInt) {
+                    toVerticalInt++;
+                    toHorizontal--;
+                } else if (toVerticalInt == fromVerticalInt) {
+                    toHorizontal--;
+                }
+            }
+
+            if (toHorizontal == getHorizontal() && toVerticalInt == fromVerticalInt) {
+                return true;
+            }
+            return false;
+        }
+        System.out.println("Данные за пределами.");
+        return false;
+    }
+
 }
