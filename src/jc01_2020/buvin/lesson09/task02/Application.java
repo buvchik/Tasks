08188f40c9e680f8bdd674f48626ba58a1d28782
buvchik <buvchik@gmail.com>
2020-02-18 20:16:34 +0300

    //done

--- src/jc01_2020/buvin/lesson09/task02/Application.java
+++ src/jc01_2020/buvin/lesson09/task02/Application.java
@@ -7,9 +7,7 @@ package jc01_2020.buvin.lesson09.task02;
  *
  */
 
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Application {
@@ -17,11 +15,11 @@ public class Application {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		// Объявить список
-		List<String> list  = new LinkedList<>();
+		LinkedList<String> list  = new LinkedList<>();
 		for (int i = 0; i < 5; i++) {
 			String str = scanner.nextLine();
 			// Заполнить список
-			list.add(0,str);
+			list.addFirst(str);
 		}
 		System.out.println("first = " + list.get(0));
 	}
