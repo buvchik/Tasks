7f44680417dbfd0f8e2b02c0dacf5f3e73f37360
buvchik <buvchik@gmail.com>
2020-02-29 12:51:25 +0300

    //done

--- src/jc01_2020/buvin/lesson12/Task2.java
+++ src/jc01_2020/buvin/lesson12/Task2.java
@@ -10,9 +10,10 @@ import java.util.stream.IntStream;
 
 public class Task2 {
 	public static void main(String[] args) {
-		int x = IntStream.range(-2, 2)
+		int x = IntStream.range(-2, 2) // -2 -1 0 1
 				// place code here
-				.reduce(10, Integer::sum);
+				.map(y->y*5)
+				.reduce(10, Integer::sum);//10+
 		System.out.println("x = " + x);
 	}
 }
