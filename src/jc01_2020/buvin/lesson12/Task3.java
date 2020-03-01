00617c1a86835f13e960673495e1cb3875bed2e4
buvchik <buvchik@gmail.com>
2020-03-01 12:33:29 +0300

    //done

--- src/jc01_2020/buvin/lesson12/Task3.java
+++ src/jc01_2020/buvin/lesson12/Task3.java
@@ -10,18 +10,39 @@ package jc01_2020.buvin.lesson12;
  *
  */
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Task3 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Map<String, String> map = new HashMap<>();
-		// full the map
+        Map<String, String> map = new HashMap<>();
+        // full the map
+        for (int i = 0; i < 20; i++) {
+            map.put("Фамилияыв" + i, "Имя" + i);
+        }
+        System.out.println(map);
 
-		String longName = null; // use map here: longName = map...
+        map.forEach((key, value) -> map.put(key, value + key));
+        System.out.println(map);
 
-		System.out.println("longName = " + longName);
 
-	}
+		List<String> result = map.entrySet()
+				.stream()
+				.map(entry -> {if (entry.getValue().length()<16)
+					return entry.getValue();
+				else
+					return "";
+				})
+				.collect(Collectors.toList());
+		System.out.println(result);
+
+		Collections.sort(result, (s1, s2) -> s2.length() - s1.length());
+		String longName = result.get(0);
+        System.out.println("longName = " + longName);
+
+    }
 }
