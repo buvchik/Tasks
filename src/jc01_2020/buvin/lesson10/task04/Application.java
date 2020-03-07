5890e9ec1b63da31deab5f0ea48806d9ec5515d9
buvchik <buvchik@gmail.com>
2020-03-07 15:39:36 +0300

    //готов

--- src/jc01_2020/buvin/lesson10/task04/Application.java
+++ src/jc01_2020/buvin/lesson10/task04/Application.java
@@ -7,16 +7,38 @@ package jc01_2020.buvin.lesson10.task04;
  *
  */
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		for (int i = 0; i < 10; i++) {
-			String surname = scanner.nextLine();
-			String firstName = scanner.nextLine();
-		}
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> nameList = new HashMap<>();
+//		for (int i = 0; i < 10; i++) {
+//			String surname = scanner.nextLine();
+//			String firstName = scanner.nextLine();
+//		}
+        nameList.put("Иванов", "Иван");
+        nameList.put("Петров", "Петр");
+        nameList.put("Сидоров", "Саша");
+        nameList.put("Лохмачев", "Вася");
+        nameList.put("Ложкин", "Саша");
+        nameList.put("Вукша", "Маша");
+        nameList.put("Пукалькина", "Лена");
+        nameList.put("Пука", "Лена");
+
+        System.out.println(nameList);
+
+        Map<String, String> copyList = new HashMap<>(nameList);
+
+        for (Map.Entry<String, String> i : nameList.entrySet()) {
+            for (Map.Entry<String, String> j : nameList.entrySet()) {
+                if (j.getValue().equals(i.getValue()) && !j.getKey().equals(i.getKey())) {
+                    copyList.remove(j.getKey());
+                }
+            }
+        }
+        System.out.println(copyList);
+    }
 
 }
