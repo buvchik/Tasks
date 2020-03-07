deefbb4690782c0f817df55316d8d0bda695cfed
buvchik <buvchik@gmail.com>
2020-03-07 16:13:07 +0300

    //готов

--- src/jc01_2020/buvin/lesson12/Task5.java
+++ src/jc01_2020/buvin/lesson12/Task5.java
@@ -8,8 +8,28 @@ package jc01_2020.buvin.lesson12;
  */
 
 public class Task5 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Кот 1", 2);
+        Cat cat2 = new Cat("Кот 2", 4);
+        myComparator myComparator = ((x, y) -> {
+            if (x.yearLife > y.yearLife)
+                return x;
+            else return y;
+        });
+        System.out.println(myComparator.compare(cat1, cat2).name);
+    }
+}
 
-	}
+class Cat {
+    String name;
+    int yearLife;
 
+    public Cat(String name, int yearLife) {
+        this.name = name;
+        this.yearLife = yearLife;
+    }
+}
+
+interface myComparator {
+    Cat compare(Cat c, Cat c1);
 }
