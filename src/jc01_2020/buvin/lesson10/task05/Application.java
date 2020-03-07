4946fd3ce836340b5135be040ca33eb8223d93a9
buvchik <buvchik@gmail.com>
2020-03-07 14:08:42 +0300

    //done

--- src/jc01_2020/buvin/lesson10/task05/Application.java
+++ src/jc01_2020/buvin/lesson10/task05/Application.java
@@ -12,13 +12,66 @@ package jc01_2020.buvin.lesson10.task05;
  *
  */
 
-public class Application {
+import java.util.*;
 
-	public static void main(String[] args) {
-		// Объявить список
-		for (int i = 0; i < 5; i++) {
-			// Заполнить список
-		}
-	}
+public class Application {
+    static class Tovar {
+        String name;
+        int count;
+
+        public Tovar(String name, int count) {
+            this.name = name;
+            this.count = count;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Объявить список
+        Map<String, List<Tovar>> map = new TreeMap<>();
+        String[] name = {"Andry", "Oly", "Valera", "Kolya"};
+        String[] tovar = {"Meat", "Chees", "Milk"};
+        Random r = new Random();
+
+        for (int i = 0; i < 40; i++) {
+            // Заполнить список
+            int nameNumber = r.nextInt(4);
+            int nameTovar = r.nextInt(3);
+            int countTovar = 1 + r.nextInt(10);
+            if (!map.containsKey(name[nameNumber]))
+                map.put(name[nameNumber], Arrays.asList(new Tovar(tovar[nameTovar], countTovar)));
+            else {
+                List<Tovar> arr = new ArrayList<>();
+                arr.addAll(map.get(name[nameNumber]));
+                boolean b = false;
+
+                for (int j = 0; j < arr.size(); j++) {
+                    if (arr.get(j).name.equals(tovar[nameTovar])) {
+                        arr.get(j).count += countTovar;
+                        b = true;
+                        break;
+                    }
+                }
+                if (!b) {
+                    arr.add(new Tovar(tovar[r.nextInt(3)], countTovar));
+                    map.put(name[nameNumber], arr);
+                } else
+                    map.replace(name[nameNumber], arr);
+            }
+        }
+
+
+        for (Map.Entry<String, List<Tovar>> item : map.entrySet()) {
+
+            System.out.println("Имя: " + item.getKey());
+            List<Tovar> arr = new ArrayList<>();
+            arr.addAll(map.get(item.getKey()));
+            for (Tovar t : arr) {
+                System.out.println(t.name + " - " + t.count);
+            }
+        }
+
+//        System.out.println(map);
+
+    }
 
 }
