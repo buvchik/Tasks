480894c40169b719f75c05480002b9dea8b5f6f8
buvchik <buvchik@gmail.com>
2020-03-28 20:20:11 +0300

    //готов

--- src/jc01_2020/buvin/lesson14/task04/Application.java
+++ src/jc01_2020/buvin/lesson14/task04/Application.java
@@ -6,10 +6,26 @@ package jc01_2020.buvin.lesson14.task04;
  *
  */
 
+import jc01_2020.buvin.lesson14.task03.Car;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class Application {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("src/jc01_2020/buvin/lesson14/resource/Car.dat");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            Car car = (Car) objectInputStream.readObject();
+            System.out.println("Name: " + car.name);
+            System.out.println("Color: " + car.color);
+            System.out.println("Price: " + car.price);
 
-	}
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
