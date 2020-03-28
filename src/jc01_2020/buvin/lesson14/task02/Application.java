5b2b8a5f9ad68f44bcb863151945a72537d2ad68
buvchik <buvchik@gmail.com>
2020-03-28 19:31:19 +0300

    //готов

--- src/jc01_2020/buvin/lesson14/task02/Application.java
+++ src/jc01_2020/buvin/lesson14/task02/Application.java
@@ -10,30 +10,35 @@ import java.io.*;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = null;
+        FileInputStream fileInputStream = null;
+        InputStreamReader inputStreamReader = null;
         try {
-            FileReader fr = new FileReader("src/jc01_2020/buvin/lesson14/task01/lesson14-01.txt");
+            fr = new FileReader("src/jc01_2020/buvin/lesson14/task01/lesson14-01.txt");
             String str = "";
             while (fr.ready()) {
                 str += (char) fr.read();
             }
 //            System.out.println(str);
             String str2 = "";
-            FileInputStream fileInputStream = new FileInputStream("src/jc01_2020/buvin/lesson14/task01/lesson14-01.txt");
-            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+            fileInputStream = new FileInputStream("src/jc01_2020/buvin/lesson14/task01/lesson14-01.txt");
+            inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
             while (inputStreamReader.ready()) {
                 str2 += (char) inputStreamReader.read();
             }
 //            System.out.println(str2);
             if (str.equals(str2)) System.out.println("Совпадают");
             else System.out.println("Не совпадают");
-            fr.close();
-            fileInputStream.close();
-            inputStreamReader.close();
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            fr.close();
+            fileInputStream.close();
+            inputStreamReader.close();
         }
     }
 
